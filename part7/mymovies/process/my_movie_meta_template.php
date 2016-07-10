fefa535fa06f553d99dee5f828e92ed9cbd4d992
deepintowp <deepintowp@gmail.com>
2016-07-10 23:25:34 +0530

    end of part 7

---
+++ part7/mymovies/process/my_movie_meta_template.php
@@ -0,0 +1,7 @@
+<ul class="movie_list_meta">
+	<li><strong>Director : </strong>DIRECTOR_PH</li>
+	<li><strong>Writer : </strong>WRITER_PH</li>
+	<li><strong>Stars : </strong>STARS_PH</li>
+	<li><strong>Genre : </strong>GENRE_PH</li>
+	<li><strong>Runtime : </strong>RUNTIME_PH</li>
+</ul>
