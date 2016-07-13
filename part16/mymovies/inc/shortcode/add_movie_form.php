529433b9f9fa69617f3387f8c1233e53f79c5af8
deepintowp <deepintowp@gmail.com>
2016-07-13 13:59:20 +0530

    end of part 16

---
+++ part16/mymovies/inc/shortcode/add_movie_form.php
@@ -0,0 +1,37 @@
+<div class="movie_status"></div>
+<form action="" id="add_new_movie">
+	<div class="form-group">
+		<label for="">Title</label>
+		<input id="sm__title" class="form-control" type="text" />
+	</div>
+	SM_CONTENT_EDITOR
+	<div class="form-group">
+		<label for="">Director</label>
+		<input id="sm__director" class="form-control" type="text" />
+	</div>
+	<div class="form-group">
+		<label for="">Writer</label>
+		<input id="sm__writer" class="form-control" type="text" />
+	</div>
+	<div class="form-group">
+		<label for="">Stars</label>
+		<input id="sm__stars" class="form-control" type="text" />
+	</div>
+	<div class="form-group">
+		<label for="">Genre</label>
+		<select name="" id="sm__genre" class="form-control">
+			<option value="comedy">Comedy</option>
+			<option value="action">Action</option>
+			<option value="thillers">Thillers</option>
+			<option value="others">Others</option>
+		</select>
+	</div>
+	<div class="form-group">
+		<label for="">Run Time</label>
+		<input id="sm__run_time" class="form-control" type="text" />
+	</div>
+	<div class="form-group">
+		
+		<button class="btn btn-primary">Sumbit Movie</button>
+	</div>
+</form>
